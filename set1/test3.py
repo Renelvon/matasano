4a7fdc8a026a0130ab60030b97ec2147632a66d1
Nikolaos Korasidis <renelvon@gmail.com>
2014-08-27 20:46:49 +0300

    Test1.5: Improve solution.

--- set1/test3.py
+++ set1/test3.py
@@ -7,7 +7,7 @@ import stringlib
 def solve(crypt_msg):
     msg_len = len(crypt_msg) // 2  # compensate for hex encoding
     max_score, decoder = 0, 'a'
-    for c in string.ascii_letters:
+    for c in string.printable:
         xor_msg = stringlib.encode_hex(c * msg_len)
         hex_msg = stringlib.xor_hexes(crypt_msg, xor_msg)
         dec_msg = stringlib.decode_hex(hex_msg)
@@ -17,11 +17,11 @@ def solve(crypt_msg):
 
     xor_msg = stringlib.encode_hex(decoder * msg_len)
     hex_msg = stringlib.xor_hexes(crypt_msg, xor_msg)
-    return stringlib.decode_hex(hex_msg)
+    return stringlib.decode_hex(hex_msg), decoder, score
 
 
 class Test(unittest.TestCase):
     def test_solve(self):
         test_in = b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
         test_out = b"Cooking MC's like a pound of bacon"
-        self.assertEqual(solve(test_in), test_out)
+        self.assertEqual(solve(test_in)[0], test_out)
