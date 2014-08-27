8b6c59ab8b37400f9044064c120d07c4f794afc2
Nikolaos Korasidis <renelvon@gmail.com>
2014-08-27 20:47:03 +0300

    Test1.4: Solved.

---
+++ set1/test4.py
@@ -0,0 +1,22 @@
+import string
+import unittest
+
+import stringlib
+import test3
+
+
+def solve():
+    l = []
+    with open("data4.txt", "r") as f:
+        best_score, the_decoder, best_msg = 0, None, None
+        for msg in f:
+            dec_msg, decoder, score = test3.solve(msg.rstrip())
+            if score > best_score:
+                best_score, the_decoder, best_msg = score, decoder, dec_msg
+    return best_score, the_decoder, best_msg
+
+
+class Test(unittest.TestCase):
+    def test_solve(self):
+        test_out = b'Now that the party is jumping\n'
+        self.assertEqual(solve()[2], test_out)
